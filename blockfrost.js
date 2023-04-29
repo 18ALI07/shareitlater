183f832b2bee6ebe9f489a01805f25ace5243525
Aliraza Malik <ahmalik_b20@it.vjti.ac.in>
2023-04-29 05:50:19 +0530

    added some extra pages

--- blockfrost.js
+++ blockfrost.js
@@ -29,109 +29,137 @@ async function runExample() {
 }
 
 runExample();
-const exampleModal = document.getElementById('exampleModal')
+// const exampleModal = document.getElementById('exampleModal')
 
-const secondModal = document.getElementById('exampleModalToggle2');
-async function loadData(arr) {
-    const url ='https://script.google.com/macros/s/AKfycbxgS9TAopiANg_7jAkC2Slf_xItyvs1SBW4LHgOqSli_bV6NcgevKn0uFVyPNOLlb-L/exec';
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      data.data.forEach(ele => {
-        arr.push(ele);
-        console.log(ele);
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-}
+// const secondModal = document.getElementById('exampleModalToggle2');
+// async function loadData(arr) {
+//     const url ='https://script.google.com/macros/s/AKfycbxgS9TAopiANg_7jAkC2Slf_xItyvs1SBW4LHgOqSli_bV6NcgevKn0uFVyPNOLlb-L/exec';
+//   return fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//       data.data.forEach(ele => {
+//         arr.push(ele);
+//         console.log(ele);
+//       });
+//     })
+//     .catch(error => {
+//       console.error('Error fetching data:', error);
+//     });
+// }
 
-function eventtoggler(){
-  alert("enter");
-  const bool=verify();
-  console.log(bool);
-  if (bool){
-    alert("true");
-    const form = document.forms['submit-to-google-sheet'];
+// function eventtoggler(){
+//   alert("enter");
+//   const bool=verify();
+//   console.log(bool);
+//   if (bool){
+//     alert("true");
+//     const form = document.forms['submit-to-google-sheet'];
     
-    form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        console.log(response);
+//     form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//         console.log(response);
       
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Network response was not ok.');
-      }
-    })
-    .then(data => {
-      console.log(data);
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         throw new Error('Network response was not ok.');
+//       }
+//     })
+//     .then(data => {
+//       console.log(data);
      
       
-    })
-    .catch(error => console.error('Error!', error.message));
-});
-exampleModal.setAttribute("data-bs-target","#exampleModalToggle2");
-  }
-  else{
-    alert("enter something");
-    exampleModal.setAttribute("data-bs-target","#exampleModalToggle1");
-  }
-  console.log(exampleModal.getAttribute("data-bs-target"));
-}
+//     })
+//     .catch(error => console.error('Error!', error.message));
+// });
+// exampleModal.setAttribute("data-bs-target","#exampleModalToggle2");
+//   }
+//   else{
+//     alert("enter something");
+//     exampleModal.setAttribute("data-bs-target","#exampleModalToggle1");
+//   }
+//   console.log(exampleModal.getAttribute("data-bs-target"));
+// }
 
 
-async function verify() {
-  let arr = [];
-  loadData(arr)
-    .then(() => {
-      console.log(arr);
-      var bool = false;
-      arr.forEach(e => {
-        if (e === document.getElementById('recipient-name').value) {
-          bool = true;
-        }
-      });
-      if(bool){
-        exampleModal.setAttribute("data-bs-target","#exampleModalToggle2");
-      }
-      else{
-        exampleModal.setAttribute("data-bs-target","#exampleModalToggle1");
-      }
-      console.log(bool);
+// async function verify() {
+//   let arr = [];
+//   loadData(arr)
+//     .then(() => {
+//       console.log(arr);
+//       var bool = false;
+//       arr.forEach(e => {
+//         if (e === document.getElementById('recipient-name').value) {
+//           bool = true;
+//         }
+//       });
+//       if(bool){
+//         exampleModal.setAttribute("data-bs-target","#exampleModalToggle2");
+//       }
+//       else{
+//         exampleModal.setAttribute("data-bs-target","#exampleModalToggle1");
+//       }
+//       console.log(bool);
      
-    console.log(bool)
+//     console.log(bool)
     
     
-      return bool;
-    })
-    .catch(error => {
-      console.error('Error verifying data:', error);
-    });
-}
+//       return bool;
+//     })
+//     .catch(error => {
+//       console.error('Error verifying data:', error);
+//     });
+// }
 
 
-const changes=document.getElementById('recipient-name');
-    changes.addEventListener('change',()=>{
+// const changes=document.getElementById('recipient-name');
+//     changes.addEventListener('change',()=>{
         
-    const bool=verify();
+//     const bool=verify();
     
-    if (bool){
+//     if (bool){
       
     
         
-} 
-else{
-  alert("bsxjbs")
-}
-console.log(exampleModal.getAttribute("data-bs-target"));
+// } 
+// else{
+//   alert("bsxjbs")
+// }
+// console.log(exampleModal.getAttribute("data-bs-target"));
 
 
-});
+// });
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzFg4zKE2M6NIrluxnHfloS8vQpyvzGcTD6zJFq9swa9sD2V-6Pf4J-TZMrH9DdT8Ll/exec';
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbzFg4zKE2M6NIrluxnHfloS8vQpyvzGcTD6zJFq9swa9sD2V-6Pf4J-TZMrH9DdT8Ll/exec';
 
 
+// const axios = require('axios');
+
+// // Set the Blockfrost API endpoint
+// const apiUrl = 'https://cardano-preprod.blockfrost.io/api/v0';
+
+// // Set your Blockfrost API key
+// const apiKey = 'preprodWM7tWLXV1F1xyDszN6Nxn8MhKtewv0Mi';
+
+// // Set the address you want to retrieve information for
+// const address = 'addr_test1qpr2lgw0sx80ca2ky7c0drtvvhdgr56r98g7xghrck0lx6ycmv0lucrznjzrux6z3vhkfvd7a6fm3rjn9rxmgr4mznssgp7yml';
+
+// // Set the API endpoint you want to call
+// const endpoint = `/addresses/${address}`;
+
+// // Set the request headers, including your API key as an authorization header
+// const headers = {
+//   'Content-Type': 'application/json',
+//   'project_id': apiKey,
+// };
+
+// // Make the API request using Axios
+// axios.get(apiUrl + endpoint, { headers: headers })
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
